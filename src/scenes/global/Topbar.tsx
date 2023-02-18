b1219a132f3c8a197241bf295ecf4d20d97db919
Fryger <krzysztof@fryger.pl>
2023-02-18 10:32:43 +0100

    Main page adjustments

--- src/scenes/global/Topbar.tsx
+++ src/scenes/global/Topbar.tsx
@@ -1,38 +1,75 @@
-import { Box, Icon, IconButton, useTheme, Typography } from "@mui/material";
-import { useContext } from "react";
+import { Box, Icon, IconButton, useTheme, Typography, Popover } from "@mui/material";
+import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material";
 import { LightModeOutlined, DarkModeOutlined, AccountCircleOutlined } from "@mui/icons-material";
 import Logo from "../../assets/logo.png"
+import LoginModal from "../../components/LoginModal";
 
 const Topbar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
     const iconStyles = {
-
         fontSize: '40px',
     };
 
 
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+
     return (
         <Box height={"64px"} display="flex" justifyContent="space-between" p={2}>
-            <Typography fontSize={"30px"}>Project URL</Typography>
-            {/* <Box
-                component="img"
-                sx={{
-                    height: 54,
-                }}
-                alt="Company logo."
-                src={Logo}
-            /> */}
-
+            <Typography fontSize={"30px"} color={colors.gray[100]}>Project URL</Typography>
             <Box display="flex" >
-                <IconButton onClick={colorMode.toggleColorMode} size="large" sx={{ height: "40px", width: "40px", marginRight: 2 }}>
+                <IconButton
+                    onClick={colorMode.toggleColorMode}
+                    sx={
+                        {
+                            height: "40px",
+                            width: "40px",
+                            marginRight: 2
+                        }
+                    }>
                     {theme.palette.mode === "dark" ? (<DarkModeOutlined style={iconStyles} />) : (<LightModeOutlined style={iconStyles} />)}
                 </IconButton>
-                <IconButton size="large" sx={{ height: "40px", width: "40px" }} ><AccountCircleOutlined style={iconStyles} /></IconButton>
+                <IconButton
+                    sx={
+                        {
+                            height: "40px",
+                            width: "40px"
+                        }}
+
+                    onClick={handleClick}
+                ><AccountCircleOutlined style={iconStyles} /></IconButton>
             </Box>
+
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}> <LoginModal /></Typography>
+            </Popover>
+
+
+
         </Box>
     )
 }
--- src/scenes/landing/index.tsx
+++ src/scenes/landing/index.tsx
@@ -1,26 +1,23 @@
 import { Box } from "@mui/system"
-import { TextField, Typography, useTheme } from "@mui/material"
-import { ColorModeContext, tokens } from "../../theme";
+import { Typography, useTheme } from "@mui/material"
+import { tokens } from "../../theme";
 import "./index.css"
 
 const Landing = () => {
-    const theme = useTheme();
 
+    const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'calc(100vh - 64px)', flexDirection: 'column' }} >
             <Box display="flex">
-                <Typography fontSize={"2.5vw"} className="full_url" />
+                <Typography fontSize={"2.5vw"} color={colors.gray[100]} className="full_url" />
             </Box>
             <Box display="flex" mt={10}>
                 <Typography fontSize={"5vw"} mx={"2%"} color={colors.primary[700]}>
-                    fryger.pl
+                    <span>fryger.pl&nbsp;/</span>
                 </Typography>
-                <Typography fontSize={"5vw"} mx={"2%"} color={colors.primary[700]}>
-                    /
-                </Typography>
-                <Typography fontSize={"5vw"} className="short_url" />
+                <Typography fontSize={"5vw"} color={colors.gray[100]} className="short_url" />
             </Box>
         </Box >
 
